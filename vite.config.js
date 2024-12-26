4f082fb8d7623d73ee8461413c0632cd132d5d9b
Pavan Roshan <119791855+varagani-pavan-roshan-krishna@users.noreply.github.com>
2024-12-27 00:12:29 +0530

    Deploy to gh-pages

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/gopikrishna/', // Make sure this matches your GitHub repository name
+  // other Vite configurations...
+});
